bfc9063489a412f89a6b8615fd2abfb977d87136
Irwin <irgranados16@gmail.com>
2020-08-10 17:07:30 -0500

    finish navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,15 +7,7 @@ export const NavBar = () => {
         <a className="navbar-brand d-lg-none d-xl-none" href="#">
           Navbar
         </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -32,21 +24,11 @@ export const NavBar = () => {
             </li>
             <li class="nav-item">
               <a class="navbar-brand" href="#">
-                <img
-                  src="https://placeimg.com/30/30/any"
-                  width="30"
-                  height="30"
-                  alt=""
-                />
+                <img src="https://placeimg.com/30/30/any" width="30" height="30" alt="" />
               </a>
             </li>
             <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
                 Contact
               </a>
             </li>
