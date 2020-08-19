8db6a8fadc1ef42d8763557cc86b71b13018396a
Irwin <irgranados16@gmail.com>
2020-08-19 14:15:29 -0500

    add react-ticker for horizontal text scrolling

--- src/components/StockPrice.js
+++ src/components/StockPrice.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Ticker from "react-ticker";
 
 export const StockPrice = () => {
   return (
-    <div className="row mt-5" id="stockPrice">
-      <ul className="list-group list-group-horizontal">
-        <li className="list-group-item small">S&P 500 Futures 3352.70 +7.900 +0.24% </li>
-        <li className="list-group-item small">WTI Future 41.98 +0.040 +0.10%</li>
-        <li className="list-group-item small">BRENT Future 44.99 +0.590 +1.33%</li>
-        <li className="list-group-item small">PDAX INDEX 12687.53 +12.650 +0.10%</li>
-        <li className="list-group-item small">NIKKEI 225 22329.94 -88.210 -0.39%</li>
-      </ul>
-    </div>
+    // using React-Ticker for moving text
+    <Ticker offset="100%">
+      {({ index }) => (
+        <div className="mt-5" id="stockPrice">
+          <ul className="list-inline">
+            <li className="list-inline-item small">• S&P 500 Futures 3352.70 +7.900 +0.24%</li>
+            <li className="list-inline-item small">• WTI Future 41.98 +0.040 +0.10%</li>
+            <li className="list-inline-item small">• NIKKEI 225 22329.94 -88.210 -0.39%</li>
+          </ul>
+        </div>
+      )}
+    </Ticker>
   );
 };
